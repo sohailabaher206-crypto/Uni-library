e367085dd748060e2f6470477f89fd9d6f7e82db
sohailabaher206-crypto <sohailabaher206@gmail.com>
2025-12-04 19:43:19 +0200

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>University Library System</h1>
+            <p>Frontend is running ✔️</p>
+        </div>
+    );
+}
+
+export default App;
